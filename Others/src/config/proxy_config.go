4e5617774f324084933a8859e825beef2db1d04e
TLWFakhriAidil <fakhriaidil72@gmail.com>
2025-08-22 23:22:21 +0800

    fix reply

--- Others/src/config/proxy_config.go
+++
@@ -1,6 +0,0 @@
-package config
-
-// ProxyConfig - Reserved for future proxy implementation
-type ProxyConfig struct {
-	// Future implementation
-}
--- go-whatsapp-web-multidevice-main/push_broadcast_fix.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\ROGSTRIX\go-whatsapp-web-multidevice-main
-git add -A
-git commit -m "Fix unused broadcastManager variable in cmd/rest.go"
-git push origin main
-echo.
-echo Push completed!
-pause
